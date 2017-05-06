2c7caa80df8144dfcdbc0adca9d05f0e7808f32a
seis <xiaonanln@gmail.com>
2017-05-06 22:38:23 +0800

    test_main

---
+++ src/test/test_main.java
@@ -0,0 +1,17 @@
+package test;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdIn;
+
+public class test_main {
+
+	public static void main(String[] args) {
+		StdDraw.circle(0.5, 0.5, 0.3);
+		StdDraw.show();
+		System.err.println(Double.POSITIVE_INFINITY);
+		System.err.println(Double.NEGATIVE_INFINITY);
+		System.err.println(Integer.MAX_VALUE);
+		System.err.println(Integer.MIN_VALUE);
+	}
+
+}
